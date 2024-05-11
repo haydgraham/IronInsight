9f031f7a9591f789d40a80f1d7ad01b12e30fda1
haydgraham <samuelh.graham@gmail.com>
2024-05-10 17:49:04 -0700

    first commit

--- IronInsight/ChartView.swift
+++ IronInsight/ChartView.swift
@@ -28,6 +28,7 @@ struct ChartView: View {
             .cornerRadius(12)
             .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
             .padding()
+            .padding()
         
     }
 }
